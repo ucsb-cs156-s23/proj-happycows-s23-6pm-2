e67ef567b0d97b0025346e1348417756e8a6cf0d
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-05-31 17:38:35 -0700

    Add story center align decorator

--- frontend/src/stories/components/Commons/CommonsCardBox.stories.js
+++ frontend/src/stories/components/Commons/CommonsCardBox.stories.js
@@ -4,8 +4,8 @@ import { CommonsCardBoxWithData } from "main/components/Commons/CommonsCardBox";
 import userCommonsFixtures from 'fixtures/userCommonsFixtures';
 
 export default {
-    title: 'components/commons/CommonsCardBox',
-    component: CommonsCardBoxWithData
+  title: 'components/commons/CommonsCardBox',
+  component: CommonsCardBoxWithData
 };
 
 const Template = (props) => <CommonsCardBoxWithData {...props} />;
@@ -13,6 +13,14 @@ const Template = (props) => <CommonsCardBoxWithData {...props} />;
 export const Default = Template.bind({});
 
 Default.args = {
-    commons: userCommonsFixtures.oneUserCommons[0].commons,
-    userCommons: userCommonsFixtures.oneUserCommons[0],
+  commons: userCommonsFixtures.oneUserCommons[0].commons,
+  userCommons: userCommonsFixtures.oneUserCommons[0],
 }
+
+export const decorators = [
+  (Story) => (
+    <div style={{ textAlign: 'center' }}>
+      <Story />
+    </div>
+  ),
+];
