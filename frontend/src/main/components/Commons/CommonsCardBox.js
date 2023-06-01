0e2ef178092c3fac63a18b2a91cd4b4545d8b4cb
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-05-31 17:13:47 -0700

    Fix story

--- frontend/src/main/components/Commons/CommonsCardBox.js
+++ frontend/src/main/components/Commons/CommonsCardBox.js
@@ -3,6 +3,45 @@ import React from "react";
 import { Card, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+// This should only be used directly for stories. Otherwise use the default export.
+export function CommonsCardBoxWithData({ commons, userCommons }) {
+  return (
+    <Card
+      data-testid={"commons-card-box-" + commons.id} style={
+        // Stryker disable all : no need to unit test CSS
+        {
+          width: '18rem',
+          margin: '0.3rem',
+          backgroundColor: 'white',
+          borderRadius: '0.5em',
+          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+        }
+        // Stryker enable all
+      }>
+      <Card.Body>
+        <Card.Title>{commons.name}</Card.Title>
+        <Card.Text style={
+          // Stryker disable next-line all : no need to unit test CSS
+          { margin: "0.2em" }
+        }>
+          Total wealth: ${userCommons.data.totalWealth}
+        </Card.Text>
+        <Card.Text>
+          Owned cows: {userCommons.data.numOfCows}
+        </Card.Text>
+        <Link to={"/play/" + commons.id} data-testid={"enter-common-" + commons.id} style={{
+          // Stryker disable next-line all : no need to unit test CSS
+          textDecoration: 'none'
+        }}>
+          <Button variant="primary">
+            Enter
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  )
+}
+
 const CommonsCardBox = ({ commons }) => {
   // Stryker disable all
   const id = commons ? commons.id : -1;
@@ -24,39 +63,7 @@ const CommonsCardBox = ({ commons }) => {
   }
 
   return (
-    <Card
-      data-testid={"commons-card-box-" + id} style={
-        // Stryker disable all : no need to unit test CSS
-        {
-          width: '18rem',
-          margin: '0.3rem',
-          backgroundColor: 'white',
-          borderRadius: '0.5em',
-          boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
-        }
-        // Stryker enable all
-      }>
-      <Card.Body>
-        <Card.Title>{commons.name}</Card.Title>
-        <Card.Text style={
-          // Stryker disable next-line all : no need to unit test CSS
-          { margin: "0.2em" }
-        }>
-          Total wealth: ${commonsData.data.totalWealth}
-        </Card.Text>
-        <Card.Text>
-          Owned cows: {commonsData.data.numOfCows}
-        </Card.Text>
-        <Link to={"/play/" + id} data-testid={"enter-common-" + id} style={{
-          // Stryker disable next-line all : no need to unit test CSS
-          textDecoration: 'none'
-        }}>
-          <Button variant="primary">
-            Enter
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <CommonsCardBoxWithData commons={commons} userCommons={commonsData} />
   )
 };
 
--- frontend/src/stories/components/Commons/CommonsCardBox.stories.js
+++ frontend/src/stories/components/Commons/CommonsCardBox.stories.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import CommonsCardBox from "main/components/Commons/CommonsCardBox";
+import { CommonsCardBoxWithData } from "main/components/Commons/CommonsCardBox";
 import userCommonsFixtures from 'fixtures/userCommonsFixtures';
 
 export default {
     title: 'components/commons/CommonsCardBox',
-    component: CommonsCardBox
+    component: CommonsCardBoxWithData
 };
 
-const Template = (props) => <CommonsCardBox {...props} />;
+const Template = (props) => <CommonsCardBoxWithData {...props} />;
 
 export const Default = Template.bind({});
 
 Default.args = {
     commons: userCommonsFixtures.oneUserCommons[0].commons,
+    userCommons: userCommonsFixtures.oneUserCommons[0],
 }
