195262e9c1e542116597bc23a65ac341e02ce9dd
Cameron Cummins <camwcummins@gmail.com>
2023-06-06 13:30:08 -0700

    small changes

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/CommonsController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/CommonsController.java
@@ -190,6 +190,7 @@ public class CommonsController extends ApiController {
         .totalWealth(joinedCommons.getStartingBalance())
         .numOfCows(0)
         .cowHealth(100)
+        .cowDeaths(0)
         .build();
 
     userCommonsRepository.save(uc);
